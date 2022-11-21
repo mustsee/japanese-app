6414a88086986d6c6c27e5fad32e57f112c8dbe8
Thomas Sypniewski <thomas@thomass-macbook-pro.home>
2022-11-21 12:29:35 +0100

    Setup gh pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import { Card, Typography } from "@mui/joy";
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
-    <Card variant="outlined" sx={{ textAlign: "center", userSelect: "none" }}>
-      <Typography>{props.title}</Typography>
-    </Card>
+    <Link to="/" style={{ textDecoration: "none" }}>
+      <Card variant="outlined" sx={{ textAlign: "center", userSelect: "none" }}>
+        <Typography>{props.title}</Typography>
+      </Card>
+    </Link>
   );
 }
 
